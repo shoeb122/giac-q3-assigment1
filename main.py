ab1d6db7ec56aee89ba649d47a035463bbb12022
shoeb122 <ahmadshoeb490@gmail.com>
2025-02-27 07:06:53 +0500

    Initial commit

---
+++ main.py
@@ -0,0 +1,49 @@
+# data types and speciacl keyword in python
+
+# string
+
+name : str ="shoeb"
+print(name)
+
+# int
+
+age: int = 17
+print(age)
+
+# list
+
+list = [1,2,3,4,"apple"]
+print(list)
+
+
+#tupples
+tupple =(1,2,3)
+
+# set 
+
+sets ={1,2,3,4,5}
+
+# boolean
+
+a = True
+b = False
+print(a)
+
+
+#special keywords in python
+
+
+#if , elif,else
+
+x = 10
+if x > 5 :
+    print("greater then 5")
+elif x == 5:
+    print("equal to 5")
+else:
+    ("less than 5")
+
+
+    #there are so many special keywords in python
+    # we will learn them in upcoming classes
+    
